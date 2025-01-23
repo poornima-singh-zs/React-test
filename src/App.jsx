4bf3f01a86deab55cf1bda6eca3f01f45e9b2c8b
Raramuri <admin@Admins-MacBook-Pro.local>
2025-01-23 12:11:52 +0530

    Added day13 morning task to build a modal in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React , { useState } from 'react'
 // import Test from './React-test Day10/Test';
 import './App.css'
 import ChildComp from '../src/React-test Day11/ChildComp'
+// import "./Day13/Modal.css"
+import Modal from '../src/Day13/Modal'
 
 
 
@@ -15,39 +17,52 @@ export default function App() {
   //   }
   // ];
 
-  const [numDice, setNumDice] = useState(0);
-  const [showDice, setShowDice] = useState(0);
-  const [error, setError] = useState('');
+//   const [numDice, setNumDice] = useState(0);
+//   const [showDice, setShowDice] = useState(0);
+//   const [error, setError] = useState('');
 
-  const handleRollDice = () => {
-    if(numDice>=1 && numDice<=6){
-      setShowDice(numDice);
-      setError('');
-  }
-  else{
-    setError('Please enter a number between 1 and 6');
-  }
-}
+//   const handleRollDice = () => {
+//     if(numDice>=1 && numDice<=6){
+//       setShowDice(numDice);
+//       setError('');
+//   }
+//   else{
+//     setError('Please enter a number between 1 and 6');
+//   }
+// }
   
-  return (
-    <div className='parent' >
+//   return (
+//     <div className='parent' >
     
-    <input
-        type="number"
-        min="1"
-        max="6"
-        value={numDice}
-        onChange={(e) => setNumDice(Number(e.target.value))}
-        placeholder="Enter number of dice"
-      />
-       {error && <p className="error">{error}</p>} 
-    <button className='roll' onClick={handleRollDice}> Roll</button>
-    <ChildComp  numDice={showDice}/>
-    {/* <Test title={items[0].title} description={items[0].description} /> */}
-    </div>
-  )
-}
+//     <input
+//         type="number"
+//         min="1"
+//         max="6"
+//         value={numDice}
+//         onChange={(e) => setNumDice(Number(e.target.value))}
+//         placeholder="Enter number of dice"
+//       />
+//        {error && <p className="error">{error}</p>} 
+//     <button className='roll' onClick={handleRollDice}> Roll</button>
+//     <ChildComp  numDice={showDice}/>
+//     {/* <Test title={items[0].title} description={items[0].description} /> */}
+//     </div>
+//   )
+// }
 
+// Day 13 Modal Test by Anirudh
+
+const [isModalOpen, setIsModalOpen] = useState(false);
+
+const openModal = () => setIsModalOpen(true);
+const closeModal = () => setIsModalOpen(false);
+
+return(
+  <Modal isModalOpen={isModalOpen} openModal={openModal} closeModal={closeModal}></Modal>
+)
+
+
+}
 
 
 
