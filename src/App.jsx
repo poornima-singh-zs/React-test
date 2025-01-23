8181914241df84800b94403ed67c09a1fd66df55
Raramuri <admin@Admins-MacBook-Pro.local>
2025-01-23 23:34:54 +0530

    Day13 task 2 to create a debounce function

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,15 @@
 
-import React , { useState } from 'react'
-// import Test from './React-test Day10/Test';
-import './App.css'
-import ChildComp from '../src/React-test Day11/ChildComp'
+import React  from 'react'
 
+import {SearchEngine } from "../src/Day13-task2/SearchEngine"
 
 
 export default function App() {
-  // const items=[
-  //   {
-  //     title:"Hello Poornima",
-  //     description: "Hello my name is poornima singh ello my name is poornima singh  ello my name is poornima singh ello my name is poornima singh  ello my name is poornima singh ello my name is poornima singh  ello my name is poornima singh ello my name is poornima singh  ello my name is poornima singh ello my name is poornima singh  ello my name is poornima singh ello my name is poornima singh  ello my name is poornima singh ello my name is poornima singh ",
-      
-  //   }
-  // ];
 
-  const [numDice, setNumDice] = useState(0);
-  const [showDice, setShowDice] = useState(0);
-  const [error, setError] = useState('');
-
-  const handleRollDice = () => {
-    if(numDice>=1 && numDice<=6){
-      setShowDice(numDice);
-      setError('');
-  }
-  else{
-    setError('Please enter a number between 1 and 6');
-  }
-}
-  
-  return (
-    <div className='parent' >
-    
-    <input
-        type="number"
-        min="1"
-        max="6"
-        value={numDice}
-        onChange={(e) => setNumDice(Number(e.target.value))}
-        placeholder="Enter number of dice"
-      />
-       {error && <p className="error">{error}</p>} 
-    <button className='roll' onClick={handleRollDice}> Roll</button>
-    <ChildComp  numDice={showDice}/>
-    {/* <Test title={items[0].title} description={items[0].description} /> */}
-    </div>
-  )
-}
+return(
+  <SearchEngine></SearchEngine>
+)
+ }
 
 
 
