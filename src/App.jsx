cb4f4c91b6d0f2a0675a02b21b479ed7cebcc75e
Raramuri <admin@Admins-MacBook-Pro.local>
2025-01-31 03:33:37 +0530

    dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 // import {SearchEngine } from "../src/Day13-task2/SearchEngine"
-import "./Day12.css"
+// import "./Day12.css"
 // import Day14 from "./Day14.jsx";
 // import "./Day14.css"
-import Day12 from "./Day12.jsx";
+// import Day12 from "./Day12.jsx";
 import React, {useState} from "react";
-import Dice from "./Dice";
+import Dice from "./Dice.jsx";
 // import App1 from "./App1.jsx";
 // import "./App1.css"
-import Throttle from "./Day14-Throttle/Throttle";
+// import Throttle from "./Day14-Throttle/Throttle";
 
 function App(){
   
