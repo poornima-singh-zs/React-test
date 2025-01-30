2e2712cc128158b403f4c97fe225c4ff0ea471f6
Raramuri <admin@Admins-MacBook-Pro.local>
2025-01-31 01:04:46 +0530

    Renamed App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
+import React from "react";
+import App1 from "./App1";
 
-import React, { useContext } from "react";
-import ChildComponent1 from "./Day14/ChildComponent1";
-import ChildComponent2 from "./Day14/ChildComponent2";
-import ChildComponent3 from "./Day14/ChildComponent3";
-import ChildComponent4 from "./Day14/ChildComponent4";
-import MainComponent from "./Day14/MainComponent";
-import {ThemeProvider} from "./Day14/ThemeProvider";
-
-
-const App = () => {
-  return (
-    <ThemeProvider>
-      <MainComponent>
-        <ChildComponent1 />
-        <ChildComponent2 />
-        <ChildComponent3 />
-        <ChildComponent4 />
-      </MainComponent>
-    </ThemeProvider>
-  );
-};
-
-export default App;
-
-
-
+export default function App(){
+    <App1/>
+}
